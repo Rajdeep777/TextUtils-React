6ee1140c16f3c24ec9e73610a61283f95d53c0df
Rajdeep <rajdeepsingh7733@gmail.com>
2022-06-28 14:37:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react'
+import Alert from './components/Alert'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const removeBodyClasses = () => {
+    document.body.classList.remove('bg-ligh')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+  }
+  const toggleMode = (cls) => {
+    removeBodyClasses()
+    document.body.classList.add('bg-' + cls)
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = '#042143'
+      showAlert(" Dark Mode has been enabled", "success")
+    }
+    else {
+      setMode('light')
+      document.body.style.background = 'white'
+      showAlert(" Light Mode has been enabled", "success")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About mode={mode} />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character
+              counter, Remove Extra Spaces" mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
-
 export default App;
